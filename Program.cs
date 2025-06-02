649b2657f0c06d790bc08e2d1196e10d7b7eb6b4
José Bernardo Dumbo <bernardodumbo@hotmail.com.br>
2025-06-03 00:08:12 +0100

    Adicionado Program.cs com mensagem de boas vindas

---
+++ Program.cs
@@ -0,0 +1,13 @@
+// Program.cs 
+using System; 
+namespace MeuPrimeiroProjetoGit 
+{ 
+class Program 
+{ 
+static void Main(string[] args) 
+{ 
+Console.WriteLine("Olá, Git e GitHub!"); 
+Console.WriteLine("Esta é a primeira alteração no meu código."); 
+} 
+} 
+}
