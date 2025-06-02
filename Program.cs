081176b557967addb03298f59453a0ec7a0b5068
José Bernardo Dumbo <bernardodumbo@hotmail.com.br>
2025-06-03 00:14:27 +0100

    Adicionada funcionalidade de saudacao personalizada

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ static void Main(string[] args)
 { 
 Console.WriteLine("Olá, Git e GitHub!"); 
 Console.WriteLine("Esta é a primeira alteração no meu código."); 
+Console.Write("Digite seu nome: "); // Nova linha 
+string nome = Console.ReadLine(); // Nova linha 
+Console.WriteLine($"Olá, {nome}! Bem-vindo ao mundo Git!"); // Nova linha 
 } 
 } 
 }
